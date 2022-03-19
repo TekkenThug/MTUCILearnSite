2a71f9eae192ed507792473e2c0fa2cace064663
Desktop in home (Matvei) <motvig31rus@gmail.com>
2022-03-19 20:11:59 +0300

    Create weekday header

---
+++ components/Weekday/weekday-header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <strong>#{{ number }}</strong> {{ time }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "weekday-header",
+
+  props: {
+    number: {
+      type: Number,
+      required: true,
+    },
+
+    time: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
