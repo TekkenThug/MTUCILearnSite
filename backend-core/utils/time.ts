1cfe658dc8dceb11f39a9f47fcbf629cb4a9e980
Desktop in home (Matvei) <motvig31rus@gmail.com>
2022-03-20 22:04:23 +0300

    Add time utilite

---
+++ backend-core/utils/time.ts
@@ -0,0 +1,5 @@
+/**
+ * Returns true, if input date is holiday
+ * @param dateToCompare - Input date
+ */
+ export const isHoliday = (dateToCompare) => Boolean(dateToCompare === 5 || dateToCompare === 6);
