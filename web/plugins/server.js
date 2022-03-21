56eda40919b7b9bd1be3d4233fd40b0076e69c77
Desktop in home (Matvei) <motvig31rus@gmail.com>
2022-03-21 16:43:44 +0300

    Upd miragejs server

--- web/plugins/server.js
+++ web/plugins/server.js
@@ -9,17 +9,19 @@ function makeServer({ environment = "development" } = {}) {
     },
 
     seeds(server) {
-      for (let i = 0; i < 5; i++) {
+      for (let i = 1; i <= 5; i++) {
         server.create("pair", {
           name: "Распределенные операционные системы",
+          number: i,
           type: "Lecture",
           teacher: "Беленькая М.Н.",
-          place: "А-204",
+          cabinet: "А-204",
         });
       }
     },
 
     routes() {
+      this.urlPrefix = 'http://localhost:3333';
       this.namespace = "api";
 
       this.get("/schedule", (schema) => {
