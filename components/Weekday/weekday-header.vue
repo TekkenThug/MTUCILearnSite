94f9b1a9275e751505fc76c2fdffbb30580ad728
Desktop in home (Matvei) <motvig31rus@gmail.com>
2022-03-22 10:26:58 +0300

    Add erase button to weekday header

--- components/Weekday/weekday-header.vue
+++ components/Weekday/weekday-header.vue
@@ -1,12 +1,31 @@
 <template>
-  <div>
-    <strong>#{{ number }}</strong> {{ time }}
+  <div class="weekday-header">
+    <div class="weekday-header__info">
+      <strong>#{{ number }}</strong> {{ time }}
+    </div>
+
+    <ui-button
+      class="weekday-header__erase"
+      color="red"
+      :disabled="eraseIsDisabled"
+      @click="onErase"
+    >
+      <trash-icon />
+    </ui-button>
   </div>
 </template>
 
 <script>
+import UiButton from "@/components/UI/ui-button.vue";
+import { TrashIcon } from "@iconicicons/vue";
+
 export default {
   name: "weekday-header",
+
+  components: {
+    UiButton,
+    TrashIcon,
+  },
 
   props: {
     number: {
@@ -18,6 +37,29 @@ export default {
       type: String,
       required: true,
     },
+
+    eraseIsDisabled: {
+      type: Boolean,
+      required: true,
+    },
+  },
+
+  methods: {
+    onErase() {
+      if (!this.eraseIsDisabled) {
+        this.$emit("onErase");
+      }
+    },
   },
 };
 </script>
+
+<style lang="sass" scoped>
+.weekday-header
+  display: flex
+  justify-content: space-between
+
+  &__erase
+    width: 25px
+    height: 25px
+</style>
