d218883841f376b199525e59bb9c04b70f6fa715
Desktop in home (Matvei) <motvig31rus@gmail.com>
2022-03-22 14:30:15 +0300

    Create full page layout

---
+++ layouts/fullpage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="layout-fullpage">
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="sass" scoped>
+.layout-fullpage
+  height: 100vh
+</style>
