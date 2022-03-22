46c49636d65b4d5d5501368fd78615b228074982
Desktop in home (Matvei) <motvig31rus@gmail.com>
2022-03-22 14:01:25 +0300

    Input component cleaning

--- components/UI/ui-input.vue
+++ components/UI/ui-input.vue
@@ -3,11 +3,11 @@
     <label>
       {{ label }}
       <input
-        class="ui-input__field"
-        type="text"
-        :placeholder="placeholder"
-        :value="value"
-        @input="$emit('input', $event.target.value)"
+          class="ui-input__field"
+          :type="type"
+          :placeholder="placeholder"
+          :value="value"
+          @input="$emit('input', $event.target.value)"
       >
     </label>
   </div>
@@ -18,36 +18,56 @@ export default {
   name: "ui-input",
 
   props: {
+    /**
+     * Input value
+     */
     value: {
       type: String,
       default: ''
     },
+
+    /**
+     * Input placeholder
+     */
     placeholder: {
       type: String,
       default: ''
     },
+
+    /**
+     * Input label
+     */
     label: {
       type: String,
       default: ''
+    },
+
+    /**
+     * Input type
+     */
+    type: {
+      type: String,
+      default: 'text',
+      validator: (value) => ['text', 'password'].includes(value)
     }
   }
 }
 </script>
 
 <style lang="sass" scoped>
-  @import "assets/styles/variables"
-  @import "assets/styles/mixins"
+@import "assets/styles/variables"
+@import "assets/styles/mixins"
 
-  .ui-input
-    @include trans
-    margin: 10px 0
+.ui-input
+  @include trans
+  margin: 10px 0
 
-    &__field
-      padding: 5px 12px
-      border-radius: 10px
-      border: 2px solid $black-1
-      width: 100%
+  &__field
+    padding: 5px 12px
+    border-radius: 10px
+    border: 2px solid $black-1
+    width: 100%
 
-      &::placeholder
-        font-size: 14px
+    &::placeholder
+      font-size: 14px
 </style>
