2e34926922c7d682aaa64bf7fc0bdada0aa216da
Desktop in home (Matvei) <motvig31rus@gmail.com>
2022-03-22 14:32:05 +0300

    Default layout cleaning

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,32 @@
 <template>
   <div class="layout">
+    <sidebar/>
+
     <div class="layout__wrapper">
-      <Nuxt />
+      <nuxt/>
     </div>
 
-    <loader v-show="hasLoader" />
+    <loader v-show="hasLoader"/>
   </div>
 </template>
 
 <script>
 import Loader from '@/components/Loader';
+import Sidebar from "@/components/Sidebar";
 
 export default {
   name: "default",
 
   components: {
     Loader,
+    Sidebar
   },
 
   computed: {
+    /**
+     * True, if loader, must be shown
+     * @returns {boolean}
+     */
     hasLoader() {
       return this.$store.state.isLoading;
     }
@@ -27,16 +35,16 @@ export default {
 </script>
 
 <style lang="sass" scoped>
-  .layout
-    min-height: 100vh
-    display: flex
-    justify-content: center
-    align-items: center
-    background-color: #FDFFFC
+.layout
+  min-height: 100vh
+  display: flex
+  justify-content: center
+  align-items: center
+  background-color: #FDFFFC
 
-    &__wrapper
-      border-radius: 8px
-      box-shadow: rgba(0, 0, 0, 0.25) 0 14px 28px, rgba(0, 0, 0, 0.22) 0 10px 10px
-      padding: 30px
-      background-color: #ffffff
+  &__wrapper
+    border-radius: 8px
+    box-shadow: rgba(0, 0, 0, 0.25) 0 14px 28px, rgba(0, 0, 0, 0.22) 0 10px 10px
+    padding: 30px
+    background-color: #ffffff
 </style>
